8bddd36f1ac070c0acb18c35373877ac70ca6800
cobra <axcaptain@126.com>
2021-06-01 10:39:25 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -24,19 +25,11 @@ const routes: Array<RouteConfig> = [
         path: '/statistics',
         component: Statistics
     },
-    // {
-    //     path: '/',
-    //     name: 'Home',
-    //     component: Home
-    // },
-    // {
-    //     path: '/about',
-    //     name: 'About',
-    //     // route level code-splitting
-    //     // this generates a separate chunk (about.[hash].js) for this route
-    //     // which is lazy-loaded when the route is visited.
-    //     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    // }
+    {
+        path:'*',
+        component:NotFound
+    },//按顺序执行
+
 ];
 
 const router = new VueRouter({
