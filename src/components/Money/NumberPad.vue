b397d5e853acbf20fd6821a533c3e687509c1827
cobra <axcaptain@126.com>
2021-06-09 21:03:56 +0800

    收集四个组件的数据到record

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -22,11 +22,12 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class NumberPad extends Vue {
-  output = '0';
+  @Prop() readonly value!:number;
+  output = this.value.toString();
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   inputContent(event:MouseEvent){
     const button = (event.target as HTMLButtonElement);
@@ -60,7 +61,7 @@ export default class NumberPad extends Vue {
     this.output = '0';
   }
   ok(){
-    return;
+    this.$emit('update:value',this.output);
   }
 }
 
