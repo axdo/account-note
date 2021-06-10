c7ca9d4458ff542331a2ef97e8257a9fd29e24c6
cobra <axcaptain@126.com>
2021-06-10 10:48:15 +0800

    添加createdAt字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Layout class-prefix="layout">
-      {{record}}
+      {{recordList}}
       <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
       <Types :value.sync="record.type" />
       <Notes @update:value="onUpdateNotes"/>
@@ -19,11 +19,14 @@ import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
 
+const recordList :Record[] = JSON.parse(window.localStorage.getItem('recordList')||'[]');
+
 type Record = {
   tags:string[],
   notes:string,
   type:string,
-  amount:number
+  amount:number //数据类型
+  createdAt?:Date //类/构造函数
 }
 
 @Component({
@@ -32,7 +35,7 @@ type Record = {
 export default class Money extends Vue {
 
   tags = ['衣', '食', '住', '行'];
-  recordList:Record[] = [];
+  recordList:Record[] = recordList;
   record:Record={tags:[],notes:'',type:'-',amount:0}
 
   onUpdateTags(value: string[]) {
@@ -43,14 +46,14 @@ export default class Money extends Vue {
     this.record.notes=value;
   }
   saveRecord(){
-    const record2 = JSON.parse(JSON.stringify(this.record))
-    this.recordList.push(record2)
-    console.log(this.recordList)
+    const record2:Record = JSON.parse(JSON.stringify(this.record))
+    record2.createdAt = new Date();
+    this.recordList.push(record2);
   }
 
   @Watch('recordList')
   onRecordListChange(){
-    window.localStorage.setItem('recordLst',JSON.stringify(this.recordList))
+    window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
   }
 }
 </script>
