37554f6701d615f953234b1621b79fde84fb6f09
cobra <axcaptain@126.com>
2021-06-17 23:23:48 +0800

    修正createId和FormItem的问题

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -6,7 +6,7 @@
       <input type="text"
              :value="value"
              @input="onValueChanged($event.target.value)"
-             :placeholder="this.placeholder">
+             :placeholder="placeholder">
     </label>
   </div>
 </template>
@@ -20,7 +20,7 @@ export default class FormItem extends Vue{
   @Prop({default:''}) readonly value!:string;
   @Prop({required:true}) fieldName!:string;
   @Prop() placeholder?:string;
-@Watch('value')
+
   onValueChanged(value:string){
   this.$emit('update:value',value)
 }
