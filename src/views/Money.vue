6e530d3e7daa39c823098b199dbf7a8809e89f0b
cobra <axcaptain@126.com>
2021-06-23 13:25:16 +0800

    封装record的操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,12 +20,9 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component, Watch} from 'vue-property-decorator';
-import recordListModel from '@/models/recordListModel';
+import {Component} from 'vue-property-decorator';
 
 
-
-const recordList = recordListModel.fetch();
 
 
 @Component({
@@ -35,7 +32,7 @@ export default class Money extends Vue {
 
   tags = window.tagList;
   // eslint-disable-next-line no-undef
-  recordList:RecordItem[] = recordList;
+  recordList = window.recordList;
   // eslint-disable-next-line no-undef
   record:RecordItem={tags:[],notes:'',type:'-',amount:0}
 
@@ -47,13 +44,9 @@ export default class Money extends Vue {
     this.record.notes=value;
   }
   saveRecord(){
-    recordListModel.create(this.record);
+    window.createRecord(this.record);
   }
 
-  @Watch('recordList')
-  onRecordListChange(){
-    recordListModel.save()
-  }
 }
 </script>
 
