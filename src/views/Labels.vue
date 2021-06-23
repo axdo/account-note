a506f95b7175a77fd0b44b353422ed251ac07ad7
cobra <axcaptain@126.com>
2021-06-23 12:13:21 +0800

    封装window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,11 @@
 <template>
-  <div >
+  <div>
     <Layout>
       <div class="tags">
         <router-link class="tag"
-            v-for="tag in tags" :key="tag.id"
-            :to="`/labels/edit/${tag.id}`">
-          <span>{{tag.name}}</span>
+                     v-for="tag in tags" :key="tag.id"
+                     :to="`/labels/edit/${tag.id}`">
+          <span>{{ tag.name }}</span>
           <Icon name="right"/>
         </router-link>
       </div>
@@ -27,50 +27,48 @@ import Button from '@/components/Button.vue';
 @Component({
   components: {Button}
 })
-export default class Lavels extends Vue{
+export default class Lavels extends Vue {
   tags = window.tagList;
 
-  createTag(){
-    const name = window.prompt('请输出标签名')
-    if(name){
-      const message = tagListModel.create(name);
-      if(message === 'duplicated'){
-        window.alert('标签名重复')
-      } else if(message==='success'){
-        window.alert('添加成功')
-      }
+  createTag() {
+    const name = window.prompt('请输出标签名');
+    if (name) {
+      window.createTag(name);
     }
-
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.tags{
+.tags {
   background: white;
   font-size: 16px;
-  > .tag{
+
+  > .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    border:1px solid #e6e6e6;
-    svg{
+    border: 1px solid #e6e6e6;
+
+    svg {
       width: 18px;
       height: 18px;
-      color:#666;
+      color: #666;
       margin-right: 16px;
     }
   }
 }
-.createTag{
+
+.createTag {
   background: #767676;
   color: white;
   border-radius: 4px;
-  border:none;
+  border: none;
   height: 40px;
   padding: 0 16px;
-  &-wrapper{
+
+  &-wrapper {
     text-align: center;
     padding: 16px;
     margin-top: 44-16px;
