9ec7f1576d04372ba5a7ab2304c0d2e5c25b172f
cobra <axcaptain@126.com>
2021-06-24 17:02:54 +0800

    Money.vue使用Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -16,11 +16,19 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
-import store from '@/store/index2';
 
-@Component
+
+@Component({
+  computed:{
+    tagList(){
+      //TODO
+      //return this.$store.fetchTags();
+      return []
+    }
+  }
+})
 export default class Tags extends Vue {
-  tagList = store.fetchTags();
+
   selectedTags: string[] = [];
 
   toogle(tag: string) {
@@ -40,7 +48,8 @@ export default class Tags extends Vue {
       return window.alert('标签名不能为空');
 
     }
-      store.createTag(name)
+    //TODO
+      // store.createTag(name)
 
   }
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
   <div>
     <Layout class-prefix="layout">
       <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-      <Types :value.sync="record.type" />
+      <Types :value.sync="record.type"/>
       <div class="notes">
         <FormItem field-name="备注"
                   placeholder="在这里输入备注"
@@ -21,29 +21,34 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
-import store from '@/store/index2';
-
 
 
 
 @Component({
-  components: {Tags, FormItem, Types, NumberPad}
+  components: {Tags, FormItem, Types, NumberPad},
+  computed: {
+    recordList() {
+      return this.$store.state.recordList;
+    }
+  }
 })
 export default class Money extends Vue {
 
 
   // eslint-disable-next-line no-undef
-  recordList = store.recordList;
-  // eslint-disable-next-line no-undef
-  record:RecordItem={tags:[],notes:'',type:'-',amount:0}
+  record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
 
+  created(){
+    this.$store.commit('fetchRecords')
+  }
 
   onUpdateNotes(value: string) {
-    this.record.notes=value;
+    this.record.notes = value;
   }
-  saveRecord(){
-    store.createRecord(this.record);
+
+  saveRecord() {
+    this.$store.commit('createRecord',this.record);
   }
 
 }
@@ -55,7 +60,8 @@ export default class Money extends Vue {
   display: flex;
   flex-direction: column-reverse;
 }
-.notes{
+
+.notes {
   padding: 12px 0;
 }
 </style>
